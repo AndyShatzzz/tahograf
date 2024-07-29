8558129ba81a05d5b25b70923c3ed0b600496eb2
Andrey Shatskikh <ponchic-09@mail.ru>
2024-07-29 17:35:32 +0300

    fix: исправлены и добавлены метатеги

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,14 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Tahoguru",
-  description: "Тахографы",
+  title: "ТАХОДРАЙВЕР - установка тахографов в Липецке и Липецкой области",
+  description:
+    "Установка тахографов в Липецке и Липецкой области, оформление карты водителя Липецке и Липецкой области, замена блока НКМ в Липецке и Липецкой области",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={inter.className}>{children}</body>
     </html>
   );
